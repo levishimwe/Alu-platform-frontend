ff9ea0f78ade2f4438fc741b07594b6eae058fa2
levishimwe <i.levis@alustudent.com>
2025-08-01 12:04:00 +0200

    update urls

--- src/hooks/useSimpleAPI.js
+++ src/hooks/useSimpleAPI.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://alu-backend-ljw75hl22-levys-projects-81b231fd.vercel.app/api';
 
 export const useSimpleAPI = () => {
   const [loading, setLoading] = useState(false);
